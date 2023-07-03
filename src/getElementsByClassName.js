10e3e645490c12e7d2eff0e28429397541ddc3e0
Daniel <dnlprk7777@gmail.com>
2023-07-03 15:23:47 -0400

    Recursion Review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var result = [];
+  //use classList to search for class name
+  //childNode to search every element w/in element
+  //return array of elements with class name
+  var innerFunction = function(currentElement) {
+    if (currentElement.classList && currentElement.classList.contains(className)) {
+      result.push(currentElement);
+    }
+    if (currentElement.hasChildNodes()) {
+      var children = currentElement.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        innerFunction(children[i]);
+      }
+    }
+  };
+  innerFunction(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
+  //check typeof obj, array
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return String(obj);
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+  if (typeof obj === 'object') {
+    var res = '';
+    if (obj === null) {
+      return 'null';
+    } else {
+      for (var key in obj) {
+        if (key === 'undefined' || key === 'functions') {
+          continue;
+        }
+        res += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        res += ',';
+      }
+      var res2 = res.slice(0, res.length - 1);
+      var returned = '{' + res2 + '}';
+      return returned;
+    }
+  }
+  return null;
 };
